fixed new eigenfunction gradients

qulati/eigensolver.py
@@ -31,7 +31,7 @@ def fitPlaneLSTSQ(XYZ):
 
 #{{{ NEW!!! calculate eigenvalue gradients
 
-def NEWgradientEigenvectors(X, Tri, vertsInFace, N, V):
+def NEWgradientEigenvectors(X, Tri, vertsInFace, V):
     """
         N : meshFine[0:N] are vertices of origianl mesh
         
@@ -53,83 +53,94 @@ def NEWgradientEigenvectors(X, Tri, vertsInFace, N, V):
 
     # FIXME: only need to do for original faces not in the extended mesh... only need to subDivide original faces too?
 
-    gradV = np.empty([3, V.shape[1], Tri.shape[0]])
+    gradV = np.empty([Tri.shape[0], 3, V.shape[1]])
 
     for face, vInF in enumerate(vertsInFace):
 
-        #print(face + 1, "/", len(vertsInFace))
-        #print("vInF:\n", vInF)
-        v = V[vInF]
-        #print("eigenfuncs:\n", v) # each column is a different eigenfunction
+        # eigenfunctions at vertices in this original face
+        phi = V[vInF]
 
-        # we'll make the transpose of A first because it's easier
-        # -------------------------------------------------------
+        # basis vectors of triangle
+        points = X[[ vInF[i] for i in [0,1,4] ]]
+        v21 = points[1] - points[0]
+        v32 = points[2] - points[1]
 
-        coords = X[vInF].T
+        n = np.cross(v21, v32)
+        n = n / np.linalg.norm(n)
+        v = v21 / np.linalg.norm(v21)
+        u = np.cross(n, v)
+
+        # change of basis matrix
+        R = np.hstack([u,v,n]).reshape(-1,3).T
+        
+        # all the points in the face rotated down into 2D
+        points = X[vInF].dot(R)
+        #print("points:", points)
+
+        # plot to check
+        #fig = plt.figure()
+        #ax = Axes3D(fig)
+        #ax.scatter(points[:,0], points[:,1], points[:,2], c = phi[:,1], cmap = "jet")
+        #plt.show()
+
+
+        # fit cubic polynomial model to eigenfunction values
+        # --------------------------------------------------
+
+        coords = points.T
         #print("coords:\n", coords) # coordinates of each vertex for the subdivided triangle
 
         # linear terms
         one = np.ones(10) # f0
         x = coords[0] # a
         y = coords[1] # b
-        z = coords[2] # c
 
         # quadratic terms
         xx = coords[0]*coords[0] # d
         yy = coords[1]*coords[1] # e
-        zz = coords[2]*coords[2] # f
         xy = coords[0]*coords[1] # g
-        yz = coords[1]*coords[2] # h
-        zx = coords[2]*coords[0] # i
 
         # cubic terms
         xxx = coords[0]*coords[0]*coords[0] # j
         yyy = coords[1]*coords[1]*coords[1] # k
-        zzz = coords[2]*coords[2]*coords[2] # l
-
         xxy = coords[0]*coords[0]*coords[1] # m
-        xxz = coords[0]*coords[0]*coords[2] # n
-        yyz = coords[1]*coords[1]*coords[2] # o
         yyx = coords[1]*coords[1]*coords[0] # p
-        zzx = coords[2]*coords[2]*coords[0] # q
-        zzy = coords[2]*coords[2]*coords[1] # r
-        xyz = coords[0]*coords[1]*coords[2] # s
 
         # stack the features together and reshape properly
-        A = np.vstack([one,x,y,z,xx,yy,zz,xy,yz,zx,xxx,yyy,zzz,xxy,xxz,yyz,yyx,zzx,zzy,xyz]).reshape(-1, 10).T
-        #print("A matrix:", A)
-        #print("Rank of A:", np.linalg.matrix_rank(A))
+        #               f0   a  b  c   d   e   f    g    h    i
+        A = np.vstack([ one, x, y, xx, yy, xy, xxx, yyy, xxy, yyx]).reshape(-1, 10).T
 
-        coeffs = np.linalg.lstsq(A, v, rcond = False)[0]
-        f0, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s = coeffs
+        coeffs = np.linalg.lstsq(A, phi, rcond = False)[0]
+        f0, a, b, c, d, e, f, g, h, i = coeffs
         #print("coeffs:", coeffs)
 
         # vertices for centroids
-        x, y, z = x[0], y[0], z[0]
-        xx, yy, zz, xy, yz, zx = xx[0], yy[0], zz[0], xy[0], yz[0], zx[0] 
-        xxx, yyy, zzz, xxy, xxz, yyz, yyx, zzx, zzy, xyz = xxx[0], yyy[0], zzz[0], xxy[0], xxz[0], yyz[0], yyx[0], zzx[0], zzy[0], xyz[0]
+        x, y = x[0], y[0]
+        xx, yy, xy = xx[0], yy[0], xy[0] 
+        xxx, yyy, xxy, yyx = xxx[0], yyy[0], xxy[0], yyx[0]
 
-        #print("Real v[c]:", v[0])
 
         # check that the function is fit at the centroid
-        centroid_f = f0 + a*x + b*y + c*z + d*xx + e*yy + f*zz + g*xy + h*yz + i*zx + j*xxx + k*yyy + l*zzz + m*xxy + n*xxz + o*yyz + p*yyx + q*zzx + r*zzy + s*xyz
+        centroid_f = f0 + a*x + b*y + c*xx + d*yy + e*xy + f*xxx + g*yyy + h*xxy + i*yyx
+        #print("Real phi[c]:", phi[0])
         #print("Predict:  ", centroid_f)
 
         # derivatives for every eigenfunction
-        dfdx = a + 2*d*x + g*y + i*z + 3*j*xx + 2*m*xy + 2*n*zx + p*yy + q*zz + s*yz
-        dfdy = b + 2*e*y + g*x + h*z + 3*k*yy + m*xx + 2*o*yz + 2*p*xy + r*zz + s*zx
-        dfdz = c + 2*f*z + h*y + i*x + 3*l*zz + n*xx + o*yy + 2*q*zx + 2*r*yz + s*xy
+        dfdx = a + 2*c*x + e*y + 3*f*xx + 2*h*xy + i*yy
+        dfdy = b + 2*d*y + e*x + 3*g*yy + h*xx + 2*i*xy
+        dfdz = np.zeros([V.shape[1]])
 
-        #print("dfdx", dfdx)
-        #print("dfdy", dfdy)
-        #print("dfdz", dfdz)
+        # rotate 2D gradients back into 3D
+        # --------------------------------
+
+        gradPhi = np.hstack([dfdx, dfdy, dfdz]).reshape(-1,V.shape[1]).T
+        gradPhi = np.einsum("ij, jk -> ik", gradPhi, np.linalg.inv(R)) 
 
         # need to stack gradV as [3, M, centroids]
-        gradV[0,:,face] = dfdx
-        gradV[1,:,face] = dfdy
-        gradV[2,:,face] = dfdz
+        gradV[face, 0, :] = gradPhi[:,0]
+        gradV[face, 1, :] = gradPhi[:,1]
+        gradV[face, 2, :] = gradPhi[:,2]
 
-        #input()
 
     
     return gradV
@@ -244,7 +255,7 @@ def gradientEigenvectors(X, Tri, vertsInFace, N, V):
 
 
 #{{{ subDivide each face into 9 triangles
-def subDivide(mesh):
+def subDivide(X, Tri):
     """My custom subdivide function.
 
        o : original vertices
@@ -266,7 +277,7 @@ def subDivide(mesh):
 
     """
  
-    X = mesh.vertices
+    mesh = pymesh.form_mesh(X, Tri)
 
     # face centroids i.e. #
     # ---------------------
@@ -585,26 +596,31 @@ def extendMesh(X, Tri, layers, holes):
 # main function
 def eigensolver(X, Tri, holes, num = 2**8, layers = 10, subdiv = 2):
 
-    original_size = X.shape[0]
+    orig_num_verts = X.shape[0]
+    orig_num_faces = Tri.shape[0]
 
     # extend the mesh
     X, Tri = extendMesh(X, Tri, layers, holes)
+    extended_num_verts = X.shape[0]
 
     # subdivide the mesh
-    mesh = pymesh.form_mesh(X, Tri)
-    newX, newTri, vertsInFace = subDivide(mesh)
+    newX, newTri, vertsInFace = subDivide(X, Tri)
 
     # solve eigenproblem on subdivided mesh
     Q, V = LaplacianEigenpairs(newX, newTri, num = num)
     
     # get the gradients at the centroids of the original mesh
-    meshFine = pymesh.form_mesh(newX, newTri)
-    gradV = NEWgradientEigenvectors(newX, newTri, vertsInFace, mesh.vertices.shape[0], V)
+    #meshFine = pymesh.form_mesh(newX, newTri)
+    gradV = NEWgradientEigenvectors(newX, newTri, vertsInFace, V)
 
     # keep values only for original (non-extended) mesh vertices
     # FIXME: which values am I keeping?
     #V, gradV = V[0:original_size], gradV[0:original_size,:,:]
 
-    return Q, V, gradV
+    # return V for original vertices + centroids, gradV was only calculated for centroids anyway
+
+    V = np.vstack( [ V[0:orig_num_verts] , V[extended_num_verts:extended_num_verts+orig_num_faces] ] )
+
+    return Q, V[0:orig_num_verts+orig_num_faces], gradV[0:orig_num_faces, :, :], newX[extended_num_verts:extended_num_verts+orig_num_faces]
 
 

qulati/gpmi.py
@@ -101,6 +101,7 @@ class AbstractModel(ABC):
             return _wrapper
     #}}}
 
+    #{{{ everything in common: init, del, reset, and abstract methods
     def __init__(self, X, Tri, Q, V, gradV):
         """Initialize class with manifold data."""
         self.X = X
@@ -148,8 +149,10 @@ class AbstractModel(ABC):
         """Return value of the mean function and gradient of mean function."""
         return self.meanFunction, self.grad_meanFunction
 
+    #}}}
     
-    # NOTE: should be an abstract method that is implemented elsewhere
+    # {{{ s2 kernel function
+    # should be an abstract method that is implemented elsewhere
     def s2kernel(self, s2i , s2j, l, grad = False):
         """Covariance between s2 values: fixed as RBF kernel for now."""
            
@@ -162,8 +165,8 @@ class AbstractModel(ABC):
             return Ks2, gradKs2
         else:
             return Ks2, None
+    #}}}
 
-    
     # {{{ data handling including scalings
     def set_data(self, y, yerr, vertex, grady = np.empty([0,3]), gradyerr = np.empty([0]), gradvertex = np.empty([0], dtype = int), s2 = None, grads2 = np.empty([0])):
         """Set data for interpolation, scaled and centred."""
@@ -182,11 +185,11 @@ class AbstractModel(ABC):
         self.s2 = s2
         self.grads2 = grads2
 
-        print(self.y)
+        #print(self.y)
         #print(self.yerr) 
-        print(self.grady)
+        #print(self.grady)
         #print(self.gradyerr)
-        input()
+        #input()
 
         self.reset()
 
@@ -208,7 +211,7 @@ class AbstractModel(ABC):
     #}}}
 
     #{{{ covariance matrix
-    def makeCovar(self, type, grad = False):
+    def makeCovar(self, type, grad = False, N = None):
         """Returns the kernel matrix of the specified type.
         
            Arguments:
@@ -239,7 +242,7 @@ class AbstractModel(ABC):
 
         if type == "prediction":
 
-            a = self.V
+            a = self.V if N is None else self.V[0:N]
             b = a
 
             # FIXME: add s2 stuff
@@ -247,9 +250,23 @@ class AbstractModel(ABC):
             b_s2 = a_s2
 
 
-        if type == "cross":
+        if type == "prediction_pointwise":
+            # NOTE: exception, just calculate pointwise variance for K(X*, X*), don't need the cross terms
+
+            SD, _ = self.spectralDensity( self.HP[0], np.sqrt(self.Q) )
 
             a = self.V
+            b = a
+
+            temp = (SD*a*b).sum(axis = 1)
+
+            return self.HP[1]*temp
+
+
+        if type == "cross":
+
+            a = self.V if N is None else self.V[0:N]
+
             b = np.vstack( [ self.V[self.vertex], self.gradV[self.gradvertex,:,:].reshape(-1,self.V.shape[1]) ] )
 
             # FIXME: add s2 stuff
@@ -517,7 +534,7 @@ class AbstractModel(ABC):
 
     #{{{ posterior distribution
     @Decorators.check_posterior_args
-    def posterior(self, s2pred = None):
+    def posterior(self, N = None, s2pred = None):
         """Calculates GP posterior mean and variance and returns *total* posterior mean and square root of pointwise variance
         
            Arguments:
@@ -527,26 +544,42 @@ class AbstractModel(ABC):
            * the returned *total* posterior mean is the sum of the GP posterior mean and the mean function
 
         """
-
-        print("Calculating posterior distribution...")
-
         # FIXME: sort out how s2pred is handled
 
-        # make covariance for data
+        # make covariance matrices
+        if N is None:
+            print("Calculating *pointwise* posterior distribution...")
+            predCov  = self.makeCovar("prediction_pointwise")
+        else:
+            print("Calculating *full* posterior distribution for first {:d} vertices...".format(N))
+            predCov  = self.makeCovar("prediction", N = N)
+
         covTrain = self.makeCovar("training")
-        predCov  = self.makeCovar("prediction")
-        crossCov = self.makeCovar("cross")
+        crossCov = self.makeCovar("cross", N = N)
 
         # NOTE: y must be prepared base on whether there are derivative observations or not
         y = self.y if self.grady is None else np.hstack([self.y, self.grady.flatten()]) 
 
-        # calculate GP posterior
+        # calculate GP posterior mean
         L = linalg.cho_factor(covTrain)
         Ef = crossCov.dot( linalg.cho_solve(L, y) )
-        Vf = predCov - crossCov.dot( linalg.cho_solve(L, crossCov.T) )
-        self.post_mean, self.post_var = Ef, Vf
+        self.post_mean = Ef
 
-        return self.meanFunction + self.unscale(Ef), self.unscale(np.sqrt(np.diagonal(Vf)), std = True)
+        # calculate GP posterior variance
+        if N is None:
+            temp = linalg.cho_solve(L, crossCov.T)
+            temp = np.einsum("ij, ji -> i", crossCov, temp) # if I transpose the K^-1 y part, will the cross term only be pointwise?
+            Vf = predCov - temp
+
+            return self.meanFunction + self.unscale(Ef), self.unscale(np.sqrt(Vf), std = True)
+
+        else:
+            
+            Vf = predCov - crossCov.dot( linalg.cho_solve(L, crossCov.T) )
+
+            self.post_var = Vf  # NOTE: only saved internal if a full posterior matrix was created, for first N vertices specified by N argument
+
+            return self.meanFunction + self.unscale(Ef), self.unscale(np.sqrt(np.diagonal(Vf)), std = True)
     #}}}
 
     #{{{ posterior distribution of gradient
@@ -632,22 +665,13 @@ class AbstractModel(ABC):
     #}}}
 
     #{{{ posteriorStatistics
-    def gradientStatistics(self, electrodes = False, s2pred = None):
+    def gradientStatistics(self, s2pred = None):
         """Calculate statistics for magnitude of posterior gradients, returns mesh idx and these statistics."""
     
-        
-        if electrodes == False: # all vertices
-            idx = list(range(self.V.shape[0]))
-            prnt = "" # "for all vertices"
-        else:
-            if s2pred is not None: # all electrodes where we have an observation for the s2 we are predicting at
-                idx = self.vertex[self.s2.flatten() == s2pred]
-                prnt = " for all electrodes with observation for s2pred"
-            else:
-                idx = self.vertex # all electrodes
-                prnt = " for all electrodes"
+        # all centroids 
+        idx = list(range(self.gradV.shape[0]))
 
-        print("Calculating posterior distribution gradient magnitudes{:s}...".format(prnt))
+        print("Calculating posterior distribution gradient magnitudes...")
         print("  (statistics: mean, stdev, 9th, 25th, 50th, 75th, 91st percentiles)")
         
         mag_stats = np.empty([len(idx), 7])
@@ -709,6 +733,7 @@ class AbstractModel(ABC):
         plt.show()
     #}}}
 
+#{{{ subclasses for different models
 
 class Matern52(AbstractModel):
     """Model y = GP(x) + e where GP(x) is a manifold GP with Matern52 kernel."""       
@@ -828,4 +853,5 @@ class basisAndMatern52(Matern52): # NOTE: inherets Matern52 class but reimplemen
         self.y_mean, self.y_std = np.mean(y), np.std(y)
         self.y, self.yerr = self.scale(y), self.scale(self.yerr, std = True)
 
+#}}}
 

qulati/gradient.py
@@ -16,14 +16,12 @@ import pymesh
 
 
 # CV stats function
-def gradient(X, Tri, scalar, at_vertices = True, magnitude_stats = True):
-    '''Calculate gradient of scalar field on every mesh *vertex* (from average of of gradients at mesh elements) for all scaler fields provided.
+def gradient(X, Tri, scalar, magnitude_stats = False):
+    '''Calculate gradient of scalar field on every mesh *face* for all scaler fields provided.
 
        NOTE:
-       * for N-vertex mesh, scalar can be (N x m) array, so calculation is performed for m different scaler fields.
+       * for N-vertex F-face mesh, scalar can be (N x m) array, so calculation is performed for m different scaler fields to get (F x n) array result.
     '''
-
-    print("[WARNING]: the conversion from gradients at face centres to gradients at vertices is not yet weighted by area...")
 
     #{{{ create mesh attributes
 
@@ -87,27 +85,32 @@ def gradient(X, Tri, scalar, at_vertices = True, magnitude_stats = True):
 
     #}}}
 
-    
+   
     #{{{ calculate "CVvertexSamples" - CV vector at vertex for each scalar sample
    
-    CVvertexSamples = np.zeros([mesh.vertices.shape[0], scalar.shape[1], 3]) # NOTE: store CV vector for all samples at every vertex
+    # NOTE: I'm not convinced that averaging to vertices is necessarily great for vectors... probably fine for magnitudes though
 
-    for s in range(scalar.shape[1]):
+    if False:
 
-        CV = CVfaceSamples[:,s,:]
+        CVvertexSamples = np.zeros([mesh.vertices.shape[0], scalar.shape[1], 3]) # NOTE: store CV vector for all samples at every vertex
 
-        vertexCVx = pymesh.convert_to_vertex_attribute(mesh, CV[:,0]).reshape([-1,1])
-        vertexCVy = pymesh.convert_to_vertex_attribute(mesh, CV[:,1]).reshape([-1,1])
-        vertexCVz = pymesh.convert_to_vertex_attribute(mesh, CV[:,2]).reshape([-1,1])
+        for s in range(scalar.shape[1]):
 
-        CVvertexSamples[:,s,:] = np.hstack([vertexCVx, vertexCVy, vertexCVz])
+            CV = CVfaceSamples[:,s,:]
+
+            vertexCVx = pymesh.convert_to_vertex_attribute(mesh, CV[:,0]).reshape([-1,1])
+            vertexCVy = pymesh.convert_to_vertex_attribute(mesh, CV[:,1]).reshape([-1,1])
+            vertexCVz = pymesh.convert_to_vertex_attribute(mesh, CV[:,2]).reshape([-1,1])
+
+            CVvertexSamples[:,s,:] = np.hstack([vertexCVx, vertexCVy, vertexCVz])
 
     #}}}
 
 
     #{{{ decide upon and return results
 
-    grad_results = CVvertexSamples if at_vertices else CVfaceSamples
+    #grad_results = CVvertexSamples if at_vertices else CVfaceSamples
+    grad_results = CVfaceSamples
 
     if magnitude_stats == False:
 